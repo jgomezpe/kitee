77330f086331e19cd7c55ff2daf59ee9cd2e47e4
Jonatan Gomez <jgomezpe@unal.edu.co>
2020-01-15 22:54:51 -0500

    Comparison service

---
+++ kitee-kompari/src/nsgl/generic/collection/IndexedComparator.java
@@ -0,0 +1,28 @@
+package nsgl.generic.collection;
+
+import nsgl.generic.Comparable;
+import nsgl.generic.Comparator;
+import nsgl.generic.Sized;
+
+public class IndexedComparator  implements Comparator{
+	public boolean eq(Indexed<?,?> one, Indexed<?,?> two) {
+		if( one instanceof Sized ) {
+			int n = ((Sized)one).size();
+			int m = ((Sized)two).size();
+			if( n!=m ) return false;
+			for( Object obj : one ) if( !Comparable.cast(one.obtain(obj)).eq(two.obtain(obj)) ) return false;
+			return true;
+		}else {	
+			for( Object obj : one ) if( !Comparable.cast(one.obtain(obj)).eq(two.obtain(obj)) ) return false; 
+			for( Object obj : two ) if( !Comparable.cast(two.obtain(obj)).eq(one.obtain(obj)) ) return false; 
+			return true;
+		}
+	}
+	
+	@Override
+	public boolean eq(Object one, Object two) {
+		if( one==two ) return true;
+		if( one instanceof Indexed ) return eq((Indexed<?,?>)one, (Indexed<?,?>)two);
+		return false;
+	}
+}
